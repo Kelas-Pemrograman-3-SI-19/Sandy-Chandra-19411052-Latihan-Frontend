f5b8338ff732a0de7c9010c432159f6806e42ad0
sandyc02 <sandychandra21@gmail.com>
2021-06-21 21:28:42 +0700

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,81 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          <label style="font-weight:bold;">SMK Apps</label>
+        </q-toolbar-title>
+
+        <div>SMK apps v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          <b style="color:blue;">Menu</b>
+        </q-item-label>
+        <q-item
+          clickable
+          :to="{ name: 'dashboard'}"
+        >
+          <q-item-section
+            avatar
+          >
+            <q-icon name="dashboard" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Dashboard</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item
+          clickable
+          :to="{ name: 'dataSiswa'}"
+        >
+          <q-item-section
+            avatar
+          >
+            <q-icon name="dashboard" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Data Siswa</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
